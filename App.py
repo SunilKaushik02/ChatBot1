43d46b74d8ed1de9b57be8a1f2d4504a14113875
Sunil Kaushik <sunil.r.kaushik88@gmail.com>
2023-07-19 17:29:18 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from langchain import LLMChain
+# from langchain import LLMChain
 # from langchain.chat_models import ChatOpenAI
 from langchain.llms import OpenAI
 # from langchain.prompts.chat import (
